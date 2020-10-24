d5db65a746fa1758a0d70d3bf5ee9297e7b4044e
testkkj <kimkj1212@naver.com>
2020-10-25 00:36:03 +0900

    마이바티스 테스트코드 실행

--- board/src/main/java/com/example/board/configuration/DatabaseConfiguration.java
+++ board/src/main/java/com/example/board/configuration/DatabaseConfiguration.java
@@ -2,7 +2,12 @@ package com.example.board.configuration;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -14,19 +19,35 @@ import com.zaxxer.hikari.HikariDataSource;
  * DatabaseConfiguration
  */
 @Configuration
-@PropertySource("classpath:/application.properties")  //application.properties를 사용할수있게 해당 파일의 주소를 지정
+@PropertySource("classpath:/application.properties") // application.properties를 사용할수있게 해당 파일의 주소를 지정
 public class DatabaseConfiguration {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.hikari")  //위의 사용할 파일에서 설정한 데이터베이스 관련정보를 사용하도록 지정
+    @ConfigurationProperties(prefix = "spring.datasource.hikari") // 위의 사용할 파일에서 설정한 데이터베이스 관련정보를 사용하도록 지정
     public HikariConfig hikariConfig() {
         return new HikariConfig();
     }
 
     @Bean
-    public DataSource dataSource() throws Exception {  //위에서 만들 설정파일을 테스트 하기위해 결과를 출력
+    public DataSource dataSource() throws Exception { // 위에서 만들 설정파일을 테스트 하기위해 결과를 출력
         DataSource dataSource = new HikariDataSource(hikariConfig());
         System.out.println(dataSource.toString());
         return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();  //스프링-마이바티스의 경우 SqlSessionFactory 생성할때 SqlSessionFactorybean을 사용
+        sqlSessionFactoryBean.setDataSource(dataSource);  //위에서 생성한 데이터소스를 설정
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:mapper/**/sql-*.xml"));  //마이바티스 맵퍼 파일 설정
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
