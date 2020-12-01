9e05ecb112ce73281040b024b9bf15160722cd6a
testkkj <46573845+testkkj@users.noreply.github.com>
2020-12-02 00:33:05 +0900

    STS4 버전 작성 시작

---
+++ board_STS4/src/main/java/board/BoardSts4Application.java
@@ -0,0 +1,13 @@
+package board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardSts4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardSts4Application.class, args);
+	}
+
+}
