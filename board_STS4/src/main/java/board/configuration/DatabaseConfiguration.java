0d5fcbdf44d77826bc9ff8311ea0725123b51f99
testkkj <46573845+testkkj@users.noreply.github.com>
2020-12-04 23:34:09 +0900

    마이바티스 적용 후에도 오류가 나는지 확인필요

--- board_STS4/src/main/java/board/configuration/DatabaseConfiguration.java
+++ board_STS4/src/main/java/board/configuration/DatabaseConfiguration.java
@@ -2,7 +2,12 @@ package board.configuration;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -14,8 +19,11 @@ import com.zaxxer.hikari.HikariDataSource;
 @PropertySource("classpath:/application.properties")
 public class DatabaseConfiguration {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Bean
-	@ConfigurationProperties(prefix="spring.datasource.hikari")
+	@ConfigurationProperties(prefix = "spring.datasource.hikari")
 	public HikariConfig hikariConfig() {
 		return new HikariConfig();
 	}
@@ -25,5 +33,19 @@ public class DatabaseConfiguration {
 		DataSource dataSource = new HikariDataSource(hikariConfig());
 		System.out.println(dataSource.toString());
 		return dataSource;
+	}
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sqlSessionFactoruBean = new SqlSessionFactoryBean();
+		sqlSessionFactoruBean.setDataSource(dataSource);
+		sqlSessionFactoruBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/sql-*.xml"));
+
+		return sqlSessionFactoruBean.getObject();
+	}
+
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
 	}
 }
