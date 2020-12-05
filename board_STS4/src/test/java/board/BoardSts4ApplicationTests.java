d239127430923ed2081176192b3601efcbab3f0f
testkkj <46573845+testkkj@users.noreply.github.com>
2020-12-05 23:33:04 +0900

    마이바티스 연결 테스트

--- board_STS4/src/test/java/board/BoardSts4ApplicationTests.java
+++ board_STS4/src/test/java/board/BoardSts4ApplicationTests.java
@@ -1,13 +1,23 @@
 package board;
 
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BoardSts4ApplicationTests {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testSqlSession() throws Exception {
+		System.out.println(sqlSessionTemplate.toString());
 	}
 
 }
